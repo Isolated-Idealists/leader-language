e77a57dae5599eda80f06407bcc8e10a8f758853
Liam Parker <parker.liam5@gmail.com>
2020-08-15 12:28:26 +1000

    Removed unused code and stubbed out basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from "react";
-import logo from "./logo.svg";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, TextField } from "@material-ui/core";
 
-const useStyles = makeStyles({
-    root: {},
+const useStyles = makeStyles((theme) => {
+    return {
+        mainContent: {
+            width: 400,
+            margin: theme.spacing(0, "auto"),
+        },
+        timelineContainer: {
+            width: "100%",
+            height: "35%",
+            position: "fixed",
+            bottom: 0,
+            backgroundColor: "#f2f2f2",
+        },
+    };
 });
 
 const App: React.FunctionComponent = () => {
     const classes = useStyles();
     return (
-        <div>
-            <header>
-                <Typography>Title or Logo</Typography>
-            </header>
-            <div>
-                <TextField />
+        <>
+            <div className={classes.mainContent}>
+                <header>
+                    <Typography align="center">Marcy waz here</Typography>
+                </header>
+                <TextField variant="filled" label="Search Term" fullWidth />
             </div>
-            <div>
-                <Typography>Cool graph</Typography>
+            <div className={classes.timelineContainer}>
+                <Typography align="center">Cool timeline thing</Typography>
             </div>
-        </div>
+        </>
     );
 };
 
